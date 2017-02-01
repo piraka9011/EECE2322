c8f97297fd223e954913091c762fb29e8a0ea05e
BlackEagleSA <alrifaie.t@husky.neu.edu>
2017-02-01 00:20:18 -0500

    Slightly modified logic bitset

--- HW2/BitSet.v
+++ HW2/BitSet.v
@@ -1,22 +1,18 @@
 module Bitset(
-		input[3:0]x,
+		input [3:0]x,
 		input [1:0]index,
 		input value,
 		output reg [3:0]y 
 		);
 
 wire [3:0]tempout;
-wire [1:0]test;
-wire [3:0] tempy;
+wire [1:0]selector;
+wire tempy;
 decoder2to4 decode(index,tempout);
-Mux2to1 mux_0 (x[tempout],test[tempout],value, tempy[0]);
-/*
-Mux2to1 mux_2 (x[1],value,y);
-Mux2to1 mux_3 (x[2],value,y);
-Mux2to1 mux_4 (x[3],value,y);
-*/
-
-
+Mux2to1 mux_0 (x[0],selector[tempout[0]],value, tempy);
+Mux2to1 mux_1 (x[1],selector[tempout[1]],value, tempy);
+Mux2to1 mux_2 (x[2],selector[tempout[2]],value, tempy);
+Mux2to1 mux_3 (x[3],selector[tempout[3]],value, tempy);
 
 endmodule 
 
@@ -29,30 +25,32 @@ assign d[3] = x[1] & x[0];
 
 endmodule
 
-module Mux2to1(input [1:0]x,input s,input value,
-output reg y);
+module Mux2to1(input x,input s,input value,
+output y);
 
-
+assign y = (s)? value:x;
 
 endmodule
 
 module Mux4to1(input [3:0]x, input [1:0]s, output y);
-assign y = x[s] ;
+assign y = x[s];
 endmodule
 
-/*
-module tb;
+module tb();
 reg [3:0]x;
 reg [1:0]index;
 reg value;
 wire [3:0]y;
 
+Bitset uut(x,index,value,y);
 initial begin
-	
+	$monitor("%d index: %d, x: %b, value:b, f:%b",$time, 
+		index,x,value,y);
 	#10;
-	index=
+	index=2'b00;
+	x=4'b0000;
+	value = 1'b1;
 end
 
 
 endmodule
-*/
