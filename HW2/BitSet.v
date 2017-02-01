71beafed68a7d8853b474977fb44cfddddf261c9
piraka9011 <piraka9011@hotmail.com>
2017-02-01 14:21:22 -0500

    Fixed 'no errors'

--- HW2/BitSet.v
+++ HW2/BitSet.v
@@ -1,74 +1,68 @@
+module Decoder2to4(input [1:0]x, 
+				   output[3:0]d ); 
+	// Shift 1 bit x times
+	assign d = 1 << x;
+
+endmodule
+
+module Mux2to1(input x,
+			   input s,
+			   input value,
+			   output y );
+
+	// Assign y to either the value (1) or x (0)
+	assign y = s ? value : x;
+
+endmodule
+
 module Bitset(
 		input [3:0]x,
 		input [1:0]index,
 		input value,
-		output [3:0]y 
-		);
+		output [3:0]y );
 
-wire [3:0]tempout;
-wire [1:0]selector;
-wire [3:0]y;
-decoder2to4 decode(index,tempout);
-Mux2to1 mux_0 (x[0],selector[tempout[0]],value, y[0]);
-Mux2to1 mux_1 (x[1],selector[tempout[1]],value, y[1]);
-Mux2to1 mux_2 (x[2],selector[tempout[2]],value, y[2]);
-Mux2to1 mux_3 (x[3],selector[tempout[3]],value, y[3]);
+	wire [3:0]tempout;
+
+	// Decode index to determine which mux to use
+	Decoder2to4 decode(index, tempout);
+	// Assign y based on Mux inputs and value
+	Mux2to1 mux_0 (x[0], tempout[0], value, y[0]);
+	Mux2to1 mux_1 (x[1], tempout[1], value, y[1]);
+	Mux2to1 mux_2 (x[2], tempout[2], value, y[2]);
+	Mux2to1 mux_3 (x[3], tempout[3], value, y[3]);
 
 endmodule 
 
-module decoder2to4(input [1:0]x, output[3:0]d);
-wire [3:0]d; 
-assign d[0] = ~x[1] & ~x[0];
-assign d[1] = ~x[1] & x[0];
-assign d[2] = x[1] & ~x[0];
-assign d[3] = x[1] & x[0];
-
-endmodule
-
-module Mux2to1(input x,input s,input value,
-output y);
-
-assign y = (s)? value:x;
-
-endmodule
-
-module Mux4to1(input [3:0]x, input [1:0]s, output y);
-assign y = x[s];
-endmodule
-
 module tb();
-reg [3:0]x;
-reg [1:0]index;
-reg value;
-wire [3:0]y;
+	reg [3:0]x;
+	reg [1:0]index;
+	reg value;
+	wire [3:0]y;
 
-Bitset uut(x,index,value,y);
+	// Call BitSet module
+	Bitset uut(x, index, value, y);
 
-initial begin
-	$monitor("%d index: %d, x: %b, value:b, f:%b",$time, 
-		index,x,index,value,y);
-	#10;
-	index=2'b00;
-	x=4'b0000;
-	value = 1;
+	initial begin
+		$monitor("%d index=%b, x=%b, value=%b, y=%b",
+				  $time, index, x, value, y);
+		#10
+		index=2'b00;
+		x=4'b1000;
+		value = 1'b1;
 
-	#10;
-	index=2'b01;
-	x=4'b0000;
-	value = 0;
+		#10
+		index=2'b01;
+		x=4'b0010;
+		value = 1'b0;
 
-	#10;
-	index=2'b10;
-	x=4'b0000;
-	value = 0;
+		#10
+		index=2'b10;
+		x=4'b0000;
+		value = 1'b1;
 
-	#10;
-	index=2'b11;
-	x=4'b0000;
-	value = 1'b0;
-
-
-end
-
-
+		#10
+		index=2'b11;
+		x=4'b1101;
+		value = 1'b0;
+	end
 endmodule
