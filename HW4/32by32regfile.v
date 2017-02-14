1099eb300444a51cdbafc8ca483f295698c2f801
piraka9011 <piraka9011@hotmail.com>
2017-02-14 11:39:15 -0500

    fixes

--- HW4/32by32regfile.v
+++ HW4/32by32regfile.v
@@ -1,106 +1,126 @@
-module RegFile32x32(
-		input write1,
-		input [31:0] write_data1,
-		input [4:0] write_index1,
-		input write2,
-		input [31:0] write_data2,
-		input [4:0] write_index2,
-		input [4:0] read_index,
-		input clear,
-		input clock,
-		output [31:0] read_value
-					);
+module RegFile32x32(input write1,
+										input [31:0] write_data1,
+										input [4:0] write_index1,
+										input write2,
+										input [31:0] write_data2,
+										input [4:0] write_index2,
+										input [4:0] read_index,
+										input clear,
+										input clock,
+										output [31:0] read_value
+										);
+										// 32x32 reg file
 
-reg [7:0] blocks [3:0];
-integer i;
+		reg [31:0] blocks [31:0];
+		integer i;
 
-always @(negedge clock, posedge clear) begin
-	if (clear) begin
-		for(i=0; i<4;i++)
-			blocks[i] = 0;		
-	end
-	else if (write1) begin
-		blocks[write_index1] = write_data1;
-	end
-	else if(write2) begin
-		blocks[write_index2] = write_data2;
-	end
-	else if(write1&write2)begin
-		blocks[write_index1] = write_data1;
-	end
-end
+		always @(negedge clock, posedge clear) begin
+			if (clear) begin
+				for(i = 0; i < 32; i = i+1)
+					blocks[i] = 0;
+			end
+			else if (write1) begin
+				blocks[write_index1] = write_data1;
+			end
+			else if(write2) begin
+				blocks[write_index2] = write_data2;
+			end
+			// Case for both write_en signals set
+			else if(write1 & write2)begin
+				blocks[write_index1] = write_data1;
+			end
+		end
 
-assign read_value = blocks [read_index];
+		assign read_value = blocks [read_index];
 
 endmodule
 
-
-
+// Testbench
 module RegFileTB;
-reg wr1,wr2;
-reg [31:0] wrd1,wrd2;
-reg [4:0] wri1, wri2, rdi;
-reg clock,clear;
-wire [31:0] rdv;
+	reg wr1,wr2;
+	reg [31:0] wrd1,wrd2;
+	reg [4:0] wri1, wri2, rdi;
+	reg clock,clear;
+	wire [31:0] rdv;
 
-RegFile32x32 uut(wr1,wrd1,wri1,wr2,wrd2,wri2,rdi,clear,clock,rdv);
+	RegFile32x32 uut(wr1,wrd1,wri1,wr2,wrd2,wri2,rdi,clear,clock,rdv);
 
-initial begin
-		clear =1;
-		#10 clear=0;
-end
+	initial begin
+			clear = 1;
+			#10
+			clear = 0;
+	end
 
-initial begin
-	clock = 1;
-	forever #5 clock = ~clock;
-end
+	initial begin
+		clock = 1;
+		forever #5 clock = ~clock;
+	end
 
-initial begin
-	$monitor("%d ", $time,
-			"clock = %d ", clock,
-			"clear = %d ", clear,	
-			"write1 = %d ", wr1,
-			"write_data1 = %d ", wrd1,
-			"write_index1 = %d ", wri1,
-			"write2 = %d ", wr2,
-			"write_data2 = %d ", wrd2,
-			"write_index2 = %d ", wri2,
-			"read_index = %d ", rdi,
-			"read_value = %d ", rdv
-			);
+	initial begin
+		$monitor("%d ", $time,
+				"clock=%d ", clock,
+				"clear=%d ", clear,
+				"write1=%d ", wr1,
+				"write_data1=%d ", wrd1,
+				"write_index1=%d ", wri1,
+				"write2=%d ", wr2,
+				"write_data2=%d ", wrd2,
+				"write_index2=%d ", wri2,
+				"read_index=%d ", rdi,
+				"read_value=%d ", rdv
+				);
 
-	#10;
-	wr1=1;
-	wr2=1;
-	wrd1 = 11;
-	wrd2 = 25; 
-	wri1 = 1;
-	wri2 = 2; 
+		#10
+		$display("Both write ports active, different registers");
+		wr1 = 1;
+		wr2 = 1;
+		wrd1 = 1081;
+		wrd2 = 2553;
+		wri1 = 0;
+		wri2 = 1;
 
-	#10;
-	wr1 = 0;
-	wr2 = 0;
-	rdi = 1;
+		#10
+		wr1 = 0;
+		wr2 = 0;
+		rdi = 0;
+		#5
+		rdi = 1;
 
-	#10;
-	rdi = 2;
+		#10
+		$display("Both write ports active, same register");
+		wr1 = 1;
+		wr2 = 1;
+		wrd1 = 10283;
+		wrd2 = 66;
+		wri1 = 2;
+		wri2 = 2;
 
-	#10;
-	wr1=1;
-	wr2=1;
-	wrd1=55;
-	wrd2=66;
-	wri1=3;
-	wri2=3;
+		#10
+		wr1 = 0;
+		wr2 = 0;
+		rdi = 2;
 
-	#10;
-	wr1 = 0;
-	wr2 = 0;
-	rdi = 3;
+		#10
+		$display("One write port active, different registers");
+		wr2 = 1;
+		wrd2 = 9969;
+		wri2 = 1;
 
+		#10
+		wr2 = 0;
+		rdi = 1;
 
-	#10 $finish;
+		#10
+		$display("One write port active, different registers");
+		wr1 = 1;
+		wrd1 = 54210;
+		wri1 = 13;
 
-end
+		#10
+		wr1 = 0;
+		rdi = 13;
 
+		#10
+		$finish;
+	end
 endmodule
--- Prelab4/reg_file.v
+++ Prelab4/reg_file.v
@@ -1,97 +1,145 @@
-module reg_file(
-				input rst,
-				input clk,
-				input wr_en,
-				input [1:0] rd0_addr,
-				input [1:0] rd1_addr,
-				input [1:0] wr_addr,
-				input [8:0] wr_data,
-				output [8:0] rd0_data,
-				output [8:0] rd1_data
-				);
-reg [8:0] register [3:0];
-integer i;
+`include "eightbit_alu.v"
+module Mux8Bit2to1(input [7:0]x1,
+									 input [7:0]x2,
+				   				 input s,
+				   		 		 output [7:0]y);
+	assign y = s ? x1 : x2;
+endmodule
 
-	always @(negedge clk, posedge rst) begin
-		if (rst) begin
-			for(i=0; i<4; i++)
-			register[i] = 0;
-			
+module reg_file(input rst,
+								input clk,
+								input wr_en,
+								input [1:0] rd0_addr,
+								input [1:0] rd1_addr,
+								input [1:0] wr_addr,
+								input [8:0] wr_data,
+								output [8:0] rd0_data,
+								output [8:0] rd1_data
+								);
+	reg [8:0] register [3:0];
+	integer i;
+
+		always @(negedge clk, posedge rst) begin
+			if (rst) begin
+				for(i=0; i<4; i=i+1)
+					register[i] = 0;
+			end
+			else if (wr_en) begin
+				register[wr_addr] = wr_data;
+			end
 		end
-		else if (wr_en) begin
-			register[wr_addr] = wr_data;			
-		end
+
+	assign rd0_data = register[rd0_addr] ;
+	assign rd1_data = register[rd1_addr] ;
+
+endmodule
+
+module tb;
+	// Reg file IO
+	reg clk, rst, wr_en;
+	reg [1:0] rd1_addr, rd0_addr, wr_addr;
+	reg [8:0] wr_data;
+	wire [8:0] rd0_data, rd1_data;
+
+	// MUX IO
+	reg [7:0] instr_i;
+	reg ALUSrc2, ALUSrc1;
+	reg [2:0] ALUOp;
+	wire [7:0] mux_out1, mux_out2;
+
+	// ALU IO
+	reg [7:0] a, b;
+	wire [7:0] f;
+	wire ovf, take_branch;
+	reg_file uut(rst, clk, wr_en, rd0_addr, rd1_addr,
+							 wr_addr, wr_data, rd0_data, rd1_data);
+  Mux8Bit2to1 mux_uut1(.x1(rd0_data[7:0]), .x2(8'b00000000), .s(ALUSrc1), .y(mux_out1));
+	Mux8Bit2to1 mux_uut2(.x1(rd1_data[7:0]), .x2(instr_i), .s(ALUSrc2), .y(mux_out2));
+	EightbitALU alu_uut(.a(mux_out1), .b(mux_out2), .s(ALUOp),
+											.f(f), .ovf(ovf), .take_branch(take_branch) );
+
+	initial begin
+			rst = 1;
+			#10
+			rst = 0;
 	end
 
-assign rd0_data = register[rd0_addr] ;
-assign rd1_data = register[rd1_addr] ;
+	initial begin
+		clk = 1;
+		forever #5
+		clk = ~clk;
+	end
 
-endmodule 
+	initial begin
+		$monitor("%d ", $time,
+				"clock=%d ", clk,
+				"clear=%d ", rst,
+				"wr_en=%d ", wr_en,
+				"rd0_addr=%d ", rd0_addr,
+				"rd1_addr=%d ", rd1_addr,
+				"wr_addr=%d ",wr_addr,
+				"wr_data=%d ", wr_data,
+				"rd0_data=%d ", rd0_data,
+				"rd1_data=%d ", rd1_data,
+				"f=%d", f,
+				);
 
-module tb; 
-reg clk,rst,wr_en;
-reg [1:0] rd1_addr,rd0_addr,wr_addr;
-reg [8:0] wr_data;
-wire [8:0] rd0_data,rd1_data;
+		/// Writing values to register file
+		#10;
+		wr_addr = 2'b00;
+		wr_data = 55;
+		rd0_addr = 2'b00;
+		rd1_addr = 2'b01;
 
-reg_file uut(rst,clk,wr_en,rd0_addr,rd1_addr,
-				wr_addr,wr_data,rd0_data,rd1_data);
+		#10;	// Enable Write
+		wr_en = 1'b1;
+		wr_addr = 2'b00;	// Address 0
+		wr_data = 9'b101010101;
 
+		#10;
+		wr_addr = 2'b01;	// Address 1
+		wr_data = 9'b010101010;
 
+		#10;
+		wr_addr = 2'b10;	// Address 2
+		wr_data = 40;
 
+		#10
+		wr_addr = 2'b11;	// Address 3
+		wr_data = 50;
 
-initial begin
-		rst =1;
-		#10 rst=0;
-end
+		/// Different MUX configurations
+		#10;	// Disable write
+		wr_en = 1'b0;
+		wr_data = 0;
+		ALUSrc1 = 1'b1;
+		ALUSrc2 = 1'b1;
+		ALUOp = 3'b000;			// Add
+		rd0_addr = 2'b00;		// Read address 0
+		rd1_addr = 2'b01;		// Read Address 1
 
-initial begin
-	clk = 1;
-	forever #5 clk = ~clk;
-end
+		#10
+		rd0_addr = 2'b01;
+		rd1_addr = 2'b00;
 
-initial begin
-	$monitor("%d ", $time,
-			"clock = %d ", clk,
-			"clear = %d ", rst,	
-			"wr_en = %d ", wr_en,
-			"read0 add. = %d ", rd0_addr,
-			"read1 add. = %d ", rd1_addr,
-			"write add. = %d ",wr_addr,
-			"write data = %d ", wr_data,
-			"read0 = %d ", rd0_data,
-			"read1 = %d ", rd1_data
-			);
+		#10
+		rd0_addr = 2'b11;
+		rd1_addr = 2'b00;
 
-	#10;
-	wr_addr =0;
-	wr_data=55;
+		#10;
+		ALUSrc1 = 1'b0;		// 0
+		ALUSrc2 = 1'b0;		// 01010101
+		ALUOp = 3'b001;		// Sub
+		instr_i = 8'b01010101;
+		rd0_addr = 2'b10;
+		rd1_addr = 2'b11;
 
-	#10;
-	wr_en = 1;
-	wr_addr = 1;
-	wr_data = 25;
+		#10
+		ALUSrc1 = 1'b1;
+		ALUSrc2 = 1'b1;
+		ALUOp = 3'b010;
 
-	#10;
-	wr_addr = 2;
-	wr_data = 30;
-
-	#10;
-	wr_addr = 3;
-	wr_data = 80;
-
-	#10;
-	wr_en=0;
-	wr_data =0;
-	rd0_addr = 0;
-	rd1_addr = 1;
-
-	#10;
-	rd0_addr = 2;
-	rd1_addr = 3;
-
-	#10 $finish;
-
-end
-
+		#10
+		$finish;
+	end
 endmodule
